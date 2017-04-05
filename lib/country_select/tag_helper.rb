d1008596824adc1e349edaca012f2db31dfd4338
nomaddc <g.freemanhl@gmail.com>
2017-04-05 21:44:11 +0300

    Revert "test"
    This reverts commit 24d053c2662b5017eafd877bdbcc1fc0df7be799.

--- lib/country_select/tag_helper.rb
+++ lib/country_select/tag_helper.rb
@@ -83,7 +83,7 @@ module CountrySelect
           if formatted_country.is_a?(Array)
             formatted_country
           else
-            formatted_country
+            [formatted_country, formatted_country]
           end
 
         end
